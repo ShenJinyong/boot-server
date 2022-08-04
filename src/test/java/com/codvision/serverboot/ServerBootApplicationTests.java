c3ffb7e10a68253751d924ded735507245f5dae0
shenjinyong <438217638@qq.com>
2022-08-04 15:41:18 +0800

    【新增】添加日志输出

--- src/test/java/com/codvision/serverboot/ServerBootApplicationTests.java
+++ src/test/java/com/codvision/serverboot/ServerBootApplicationTests.java
@@ -1,13 +1,24 @@
 package com.codvision.serverboot;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ServerBootApplicationTests {
 
+    //记录器
+    public static final Logger LOGGER = LoggerFactory.getLogger(ServerBootApplicationTests.class);
+    
     @Test
-    void contextLoads() {
+    public void contextLoads() {
+        // 打印日志信息
+        LOGGER.error("error");
+        LOGGER.warn("warn");
+        LOGGER.info("info"); // 默认日志级别
+        LOGGER.debug("debug");
+        LOGGER.trace("trace");
     }
 
 }
