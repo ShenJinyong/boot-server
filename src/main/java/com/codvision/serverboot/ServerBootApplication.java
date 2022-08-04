4c7d3cedb3f2d34490a2d127bfe8b8c3ff00a5ba
shenjinyong <438217638@qq.com>
2022-08-04 11:54:47 +0800

    【新增】初始化ServerBoot仓库

---
+++ src/main/java/com/codvision/serverboot/ServerBootApplication.java
@@ -0,0 +1,13 @@
+package com.codvision.serverboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codvision/serverboot/ServerBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codvision.serverboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
