af1a77cc4a3d8ec4a631c24e1f661be16798a08d
shenjinyong <438217638@qq.com>
2022-08-05 16:20:02 +0800

    【新增】切换uat环境，使用PostgreSQL作为数据源

--- src/main/java/com/codvision/serverboot/dao/User.java
+++ src/main/java/com/codvision/serverboot/dao/User.java
@@ -1,5 +1,6 @@
 package com.codvision.serverboot.dao;
 
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,6 +13,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@TableName("server_user")
 public class User {
 
     private String id;
