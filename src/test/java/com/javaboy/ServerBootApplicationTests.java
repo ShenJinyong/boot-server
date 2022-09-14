fe2b809fb8d966fb395293de7f3cade198333e97
shenjinyong <438217638@qq.com>
2022-09-14 11:22:34 +0800

    【优化】初始化ServerBoot脚手架

---
+++ src/test/java/com/javaboy/ServerBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaboy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
