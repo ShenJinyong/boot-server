7ca6c75d45029cd6aa61361efe8270fb0002a125
ShenJinyong <438217638@qq.com>
2023-02-27 20:24:27 +0800

    【优化】在线接口文档在开发和测试环境激活

--- src/main/java/com/example/knife4j/config/Knife4jConfiguration.java
+++ src/main/java/com/example/knife4j/config/Knife4jConfiguration.java
@@ -2,28 +2,25 @@ package com.example.knife4j.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 @Configuration
 @EnableSwagger2WebMvc
+@Profile({"dev","test"})
 public class Knife4jConfiguration {
 
     @Bean(value = "dockerBean")
     public Docket dockerBean() {
         //指定使用Swagger2规范
         Docket docket=new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(new ApiInfoBuilder()
-                        //描述字段支持Markdown语法
-                        .description("# Knife4j RESTful APIs")
-                        .termsOfServiceUrl("https://doc.xiaominfo.com/")
-                        .contact("xiaoymin@foxmail.com")
-                        .version("1.0")
-                        .build())
+                .apiInfo(apiInfo())
                 //分组名称
                 .groupName("用户服务")
                 .select()
@@ -32,5 +29,15 @@ public class Knife4jConfiguration {
                 .paths(PathSelectors.any())
                 .build();
         return docket;
+    }
+
+    private ApiInfo apiInfo(){
+        return new ApiInfoBuilder()
+                //描述字段支持Markdown语法
+                .description("# Knife4j RESTful APIs")
+                .termsOfServiceUrl("https://doc.xiaominfo.com/")
+                .contact("xiaoymin@foxmail.com")
+                .version("1.0")
+                .build();
     }
 }
